40c83599b9852ae3a82cfb1fb9fcb48a117de155
pepe <antonio.garcia.lattice@gmail.com>
2024-10-11 22:08:21 -0600

    install.sh: download and install library

---
+++ install.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+libname=constants
+
+create_directory()
+{
+  if [ -d $1 ]
+  then
+    echo "$1 directory exists."
+  else
+    echo "Creating $1 directory"
+    mkdir $1
+  fi
+}
+
+create_directory $HOME/Fortran
+
+create_directory $HOME/Fortran/lib
+
+create_directory $HOME/Fortran/include
+
+git clone https://github.com/JoseAntonioLattice/constants.git $HOME/Fortran/lib/$libname
