125b25f1443e31ac9b876b24d977ada37285eb42
pepe <antonio.garcia.lattice@gmail.com>
2024-10-11 21:51:07 -0600

    Library created. Math and Physics  constants

---
+++ test.f90
@@ -0,0 +1,14 @@
+program test
+
+  use math
+
+  implicit none
+
+  print*, euler_mascheroni
+  print*, G_newton
+  print*, h
+  print*, hbar
+  print*, h_mevs
+  print*, hbarC_MeVfm
+  
+end program test
